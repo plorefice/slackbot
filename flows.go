056649c3f94de3ea9b0ac17931618f485f37b338
Pietro Lorefice <pietro.lorefice@gmail.com>
2017-11-03 09:27:20 +0100

    Add some docs to flows

--- flows.go
+++ flows.go
@@ -7,33 +7,56 @@ import (
 	"github.com/nlopes/slack"
 )
 
+// FlowAction is the state callback. It is called whenever a new message
+// is received, and contains the user context. Return true to perform a
+// transition to the next state.
 type FlowAction func(*Bot, *slack.Msg, interface{}) bool
-type Trigger func(*Bot, *slack.Msg) bool
 
+// Guard is called to check if the associated flow can be activated. It is
+// called after all the filters. Return true to enable the flow.
+type Guard func(*Bot, *slack.Msg) bool
+
+// State represents a specific point in the execution of a flow.
 type State struct {
 	name string
 	act  FlowAction
 	dst  string
 }
 
+// StateBuilder implements the builder pattern for State objects.
 type StateBuilder struct {
 	state *State
 }
 
+// To defines the state to which the flow should transition if the state callback
+// for this state allows it.
 func (sb *StateBuilder) To(state string) *StateBuilder {
 	sb.state.dst = state
 	return sb
 }
 
+// Build assembles a new State.
 func (sb *StateBuilder) Build() *State {
-	return sb.state
+	return &State{
+		name: sb.state.name,
+		act:  sb.state.act,
+		dst:  sb.state.dst,
+	}
 }
 
+// flowContext contains all those intstance-specific parts of a flow. It is
+// the only part of the flow that must be duped when allocating a new flow instance.
 type flowContext struct {
 	currentState *State
 	userCtx      interface{}
 }
 
+// Flow is the building block of a flow-based Slackbot. It describes the sequence
+// of states the bot should go through in order to complete a certain task.
+// A flow is described by a well-defined sequence of states, a filter to ignore
+// unwanted messages, and a guard that acts as a conditional access to the flow.
+// Each instance of the flow has an associated user context that will be shared
+// among all the states belonging of the same flow.
 type Flow struct {
 	bot *Bot
 
@@ -42,16 +65,19 @@ type Flow struct {
 	initialState *State
 	userCtxTmpl  interface{}
 
-	trigger Trigger
-	filter  Filterer
+	guard  Guard
+	filter Filterer
 
 	ctx *flowContext
 }
 
+// FlowBuilder implements the builder pattern for Flow objects.
 type FlowBuilder struct {
 	flow *Flow
 }
 
+// RegisterFlow associates a flow with a bot. After registration, each new
+// non-filtered message will trigger the flow guard, if no other flows are active.
 func (bot *Bot) RegisterFlow(f *Flow) error {
 	if _, exists := bot.registeredFlows[f.name]; exists {
 		return errors.New("flow already registered")
@@ -67,7 +93,7 @@ func (bot *Bot) findFlow(ev *slack.MessageEvent) *Flow {
 	}
 
 	for _, f := range bot.registeredFlows {
-		if f.filter.filter(&ev.Msg) && f.trigger(bot, &ev.Msg) {
+		if f.filter.filter(&ev.Msg) && f.guard(bot, &ev.Msg) {
 			nf := f.dup()
 
 			bot.activeFlows[ev.User] = nf
@@ -78,10 +104,13 @@ func (bot *Bot) findFlow(ev *slack.MessageEvent) *Flow {
 	return nil
 }
 
+// NewFlow creates a new flow with an empty user context.
 func NewFlow(name string) *FlowBuilder {
 	return NewFlowWithContext(name, nil)
 }
 
+// NewFlowWithContext creates a new flow using ctx as the type template for
+// allocating each flow instance's user context.
 func NewFlowWithContext(name string, ctx interface{}) *FlowBuilder {
 	return &FlowBuilder{
 		flow: &Flow{
@@ -91,6 +120,7 @@ func NewFlowWithContext(name string, ctx interface{}) *FlowBuilder {
 	}
 }
 
+// NewState creates a new state with an associated state action.
 func NewState(name string, act FlowAction) *StateBuilder {
 	return &StateBuilder{
 		state: &State{
@@ -100,6 +130,7 @@ func NewState(name string, act FlowAction) *StateBuilder {
 	}
 }
 
+// AddStates associates the given states to the flow.
 func (fb *FlowBuilder) AddStates(states ...*State) *FlowBuilder {
 	for _, state := range states {
 		if _, exists := fb.flow.states[state.name]; !exists {
@@ -109,16 +140,19 @@ func (fb *FlowBuilder) AddStates(states ...*State) *FlowBuilder {
 	return fb
 }
 
-func (fb *FlowBuilder) SetTrigger(t Trigger) *FlowBuilder {
-	fb.flow.trigger = t
+// SetGuard associates the given guard to the flow.
+func (fb *FlowBuilder) SetGuard(g Guard) *FlowBuilder {
+	fb.flow.guard = g
 	return fb
 }
 
+// FilterBy associates the given filter to the flow.
 func (fb *FlowBuilder) FilterBy(f Filterer) *FlowBuilder {
 	fb.flow.filter = f
 	return fb
 }
 
+// Build assembles a new Flow.
 func (fb *FlowBuilder) Build(initialState string) *Flow {
 	validInitialState := false
 
@@ -148,10 +182,12 @@ func (f *Flow) step(ev *slack.MessageEvent) {
 
 func (f *Flow) dup() *Flow {
 	nf := f
+	nf.ctx = new(flowContext)
 
-	nf.ctx = &flowContext{
-		currentState: f.initialState,
-		userCtx:      reflect.New(reflect.TypeOf(nf.userCtxTmpl)).Interface(),
+	nf.ctx.currentState = f.initialState
+
+	if nf.userCtxTmpl != nil {
+		nf.ctx.userCtx = reflect.New(reflect.TypeOf(nf.userCtxTmpl)).Interface()
 	}
 
 	return nf
